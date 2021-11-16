24ef980cc46cda41079a2deb52b181001e052683
tohein <tbheinen@gmail.com>
2021-11-16 18:42:14 +0100

    Updated setup.py

--- scdali/__init__.py
+++ scdali/__init__.py
@@ -1,3 +1,10 @@
 from scdali.run_scdali_tests import run_tests
 from scdali.run_scdali_interpolation import run_interpolation
 
+__version__ = '0.1.0'
+
+__all__ = [
+    'run_tests',
+    'run_interpolation'
+]
+
--- setup.py
+++ setup.py
@@ -1,32 +1,7 @@
-import setuptools
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="scdali",
-    version="0.0.1",
-    author="Tobi Heinen",
-    author_email="t.heinen@dkfz.de",
-    description="Test for allelic imbalance in single cells.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tohein/dali",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        "numpy",
-        "scipy",
-        "chiscore",
-        "gpflow",
-        "joblib",
-        "statsmodels",
-        "tqdm",
-        "numpy_sugar"
-    ]
-)
+if __name__ == "__main__":
+    setup()
+
